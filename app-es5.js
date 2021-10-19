ae187cae6246a3366127e66113c465b6abde0a37
Goldengide <amowogbajegideon@gmail.com>
2021-10-19 16:37:02 +0100

    mongoose correction

--- app-es5.js
+++ app-es5.js
@@ -8,11 +8,10 @@ const {bookRoutes, casualRoutes} = require('./src/es5/routes/bookRoutes');
 
 const app = express();
 const PORT = 3020;
-
+const uri_remote = "mongodb+srv://bibledb:waffi-alexander@cluster0.i0qv2.mongodb.net/bibledb";
+const uri_local = "mongodb://localhost/bibledb";
 mongoose.Promise = global.Promise;
-let uri = "mongodb+srv://bibledb:waffi-alexander@cluster0.i0qv2.mongodb.net/test"
-
-mongoose.connect(uri, {
+mongoose.connect(uri_remote, {
     useNewUrlParser: true,
     useUnifiedTopology:true
 });
@@ -35,7 +34,7 @@ app.use(function (req, res, next) {
 
 //  Routes
 bookRoutes(app);
-// casualRoutes(app);
+casualRoutes(app);
 // versesRoutes(app);
 app.get('/', (req, res) =>
     res.json(`Store server running on port ${PORT}`)
